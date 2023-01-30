b0a8f0e0d3b3e5a855f98bd053cc4589b0bc12a3
JulianFernandini <jfernandini@zagmail.gonzaga.edu>
2023-01-29 16:47:57 -0800

    Hello World

--- src/demi.java
+++ src/demi.java
@@ -3,6 +3,6 @@ public class demi {
     public static void main(String[] args) {
         System.out.println("Helloooo World");
         System.out.println("");
-        System.out.println("hi ");
+        System.out.println("hi string");
     }
 }
